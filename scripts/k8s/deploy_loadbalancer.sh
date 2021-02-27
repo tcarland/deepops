9eb3807c1288e4af4edd2339aa5c5b8b60f48c15
tca <tcarland@gmail.com>
2021-02-27 05:44:11 -0800

    update ns used by metallb update gitignore to save our branch config

--- scripts/k8s/deploy_loadbalancer.sh
+++ scripts/k8s/deploy_loadbalancer.sh
@@ -37,6 +37,6 @@ fi
 
 # Set up the MetalLB load balancer
 if ! helm status metallb >/dev/null 2>&1; then
-	kubectl create namespace deepops-loadbalancer
-	helm install --wait metallb bitnami/metallb "${helm_install_args[@]}" --version ${HELM_METALLB_CHART_VERSION} --namespace deepops-loadbalancer
+	#kubectl create namespace deepops-loadbalancer
+	helm install --wait metallb bitnami/metallb "${helm_install_args[@]}" --version ${HELM_METALLB_CHART_VERSION}
 fi
